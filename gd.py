f9a23107394768d1f62f2af92d4398bfd4ed67ac
gibaoxi <100911189+gibaoxi@users.noreply.github.com>
2024-01-10 23:53:49 +0800

    Update gd.py

--- gd.py
+++ gd.py
@@ -102,8 +102,6 @@ print(response4.text)
 response5= requests.get(url=url5)
 print(response5.text)
 
-response6= requests.get(url=url6,headers=headers1) 
-print(response6.text)
 
 response7= requests.get(url=url7)  
 print(response7.text) 
