bc83baa18a5fd0dce07dcf32fbcf76d9f3b166ba
gibaoxi <100911189+gibaoxi@users.noreply.github.com>
2024-01-11 14:09:12 +0800

    Update gd.py

--- gd.py
+++ gd.py
@@ -4,7 +4,7 @@ url1="https://glitchyyb.deno.dev";    #url="";
 
 url2="https://ggyyb.deno.dev";
 
-url3="https://jhs48h-3000.csb.app'';
+url3="https://jhs48h-3000.csb.app"; 
 
 url4="https://47cds5-3000.csb.app";
 
