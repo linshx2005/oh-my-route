5de2fb6ce2ceda71999f5a24176ac0940f5c1da7
gibaoxi <100911189+gibaoxi@users.noreply.github.com>
2024-01-22 21:27:54 +0800

    Update gd.py

--- gd.py
+++ gd.py
@@ -19,7 +19,7 @@ url10="https://ad65a318-9394-4984-9149-ab12145436fa-00-2mgnuhqlp9rp6.janeway.rep
 url8="https://3624tj-3001.csb.app/";
 
 url9="https://possible-rich-aquarius.glitch.me/";
-urla="https://yyb.onwebapp.io/";
+url11="https://yyb.onwebapp.io/";
 
 
 
@@ -143,8 +143,8 @@ print(response3.text)
 response9= requests.get(url=url9)
 print(response9.text)
 
-responsea= requests.get(url=urla)
-print(responsea.text)
+response11= requests.get(url=url11)
+print(response11.text)
 '''response= requests.get(url=url)
 print(response.text)'''
 
