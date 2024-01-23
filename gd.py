1dc5ac886410f827c0f262e77a1d61c5d108f9b4
gibaoxi <100911189+gibaoxi@users.noreply.github.com>
2024-01-23 12:21:42 +0800

    Update gd.py

--- gd.py
+++ gd.py
@@ -20,7 +20,7 @@ url8="https://3624tj-3001.csb.app/";
 
 url9="https://possible-rich-aquarius.glitch.me/";
 
-url1="https://670e6400-af12-446c-954d-5086f32ffae7-00-2qkt1mg7eoe7n.janeway.replit.dev/";
+url11="https://670e6400-af12-446c-954d-5086f32ffae7-00-2qkt1mg7eoe7n.janeway.replit.dev/";
 
 
 headers4={
