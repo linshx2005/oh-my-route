344c5e150f82ee027e6d3a3a07ed2196ca0d0a8c
gibaoxi <100911189+gibaoxi@users.noreply.github.com>
2024-03-07 19:01:34 +0800

    Update dk.py

--- dk.py
+++ dk.py
@@ -1,17 +1,20 @@
 import requests
-  
-def send_server_chan_notification(access_token, title, content):  
-    """  
-    向Server酱发送POST通知  
-  
-    :param access_token: Server酱的access_token  
-    :param title: 通知的标题  
-    :param content: 通知的内容  
-    """  
+from datetime import datetime
+
+dt = datetime.now()
+dt = dt.strftime('%j')
+dt = int(dt)
+print(dt)
+l = [50,60]
+l.append(dt)
+print(l)
+x = l[-1] - l[-2]
+print(x)
+def send_server_chan_notification(access_token, title, content):
     url = "https://sctapi.ftqq.com/" + access_token + ".send"  
     data = {  
         "text": content,  
-        "desp": content,  # 有些Server酱版本可能需要这个字段  
+        "desp": content,  
     }  
     if title:  
         data["title"] = title  
@@ -23,10 +26,10 @@ def send_server_chan_notification(access_token, title, content):
     response = requests.post(url, data=data, headers=headers)
     
 def push_message(title, content):
-    # PushPlus API Token
+    
     api_token = 'c204e4622c9f4e3e8bf06591c7f6e89d'
  
-    # PushPlus请求URL
+    
     url = 'http://www.pushplus.plus/send'
  
     # 发送POST请求
@@ -46,8 +49,12 @@ def push_message(title, content):
         print("消息推送出错:", e)    
 # 使用示例  
 if __name__ == "__main__":  
-    ACCESS_TOKEN = "SCT227782TMxJlx5YE887DkDfBcQ93prou"  
-    TITLE = "dk6776513150dk6753671485"  
-    CONTENT = "dk6776513150dk6753671485"
-    push_message(TITLE, CONTENT)
-    send_server_chan_notification(ACCESS_TOKEN, TITLE, CONTENT)
+    if x == 7:
+        ACCESS_TOKEN = "SCT227782TMxJlx5YE887DkDfBcQ93prou"  
+        TITLE = "dkgibaoxidk243566"  
+        CONTENT = "vc6776513150vc6753671485"
+        push_message(TITLE, CONTENT)
+        send_server_chan_notification(ACCESS_TOKEN, TITLE, CONTENT)
+    else:
+        print(1)    
+  
