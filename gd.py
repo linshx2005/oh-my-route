bf64a41b1afe315094d5f3964ea4d49692b352ef
gibaoxi <100911189+gibaoxi@users.noreply.github.com>
2024-04-19 10:34:23 +0800

    Add files via upload

--- gd.py
+++ gd.py
@@ -1,31 +1,4 @@
 import requests
-from datetime import datetime  
-lis = './lis.json'
-dk = 'dk.json'
-# 获取今天的日期  
-today = datetime.today()  
-  
-# 获取今天是今年的第几天  
-day = today.timetuple().tm_yday  
-
-try:  
-    with open(lis, 'r') as f:  
-        li = eval(f.read())  
-except FileNotFoundError:  
-    print('1')
-day2 = day - 9
-li.append(day)
-with open(lis, 'w') as f:  
-    f.write(str(li))
-try:  
-    with open(dk, 'r') as f:  
-        du = eval(f.read())  
-except FileNotFoundError:  
-    print('2')
-day1 = day - 6
-du.append(day)
-with open(dk, 'w') as f:  
-    f.write(str(du))
   
 def send_server_chan_notification(access_token, title, content):  
     """  
@@ -73,12 +46,8 @@ def push_message(title, content):
         print("消息推送出错:", e)    
 # 使用示例  
 if __name__ == "__main__":  
-    ACCESS_TOKEN = "SCT242760TeNxiWHrdLtFYqmJkeQnqE2Xq"  
-    TITLE = "vc"
-    TITLE1 = "dk"  
-    CONTENT = "vc\n6776513150\n6753671485"
-    CONTENT1 = 'dk\n6776513150\n6753671485'
-    li.append(day)
-    del li[0]
-    with open(lis, 'w') as f:  
-        f.write(str(li))        
+    ACCESS_TOKEN = "SCT227782TMxJlx5YE887DkDfBcQ93prou"  
+    TITLE = "vc"  
+    CONTENT = "6776513150\n6753671485"
+    push_message(TITLE, CONTENT)
+    send_server_chan_notification(ACCESS_TOKEN, TITLE, CONTENT)
